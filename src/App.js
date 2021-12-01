67452996ec74044be5716cf8408c8c82d8592e9e
Russ Safin <russ.safin@gmail.com>
2021-12-01 02:12:06 -0800

    add colorHelper to create colors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Palette from "./Palette";
 import seedColors from "./seedColors";
+import {generatePalette} from "./colorHelpers";
 
 function App() {
-  return (
-    <div>
-      <Palette {...seedColors[4]} />
-    </div>
-  );
+    console.log(generatePalette(seedColors[4]));
+    return (
+        <div>
+            <Palette {...seedColors[4]} />
+        </div>
+    );
 }
 
 export default App;
