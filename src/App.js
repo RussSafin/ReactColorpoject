99eaf7c69061dbc067be272ef8d2792882fea078
Russ Safin <russ.safin@gmail.com>
2021-12-02 23:53:37 -0800

    add Link to ColorBox and new Route

--- src/App.js
+++ src/App.js
@@ -6,18 +6,21 @@ import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
 class App extends Component {
-	findPalette (id) {
-		return seedColors.find(function(palette) {
+	findPalette(id) {
+		return seedColors.find(function (palette) {
 			return palette.id === id;
 		});
 	}
 	render() {
 		return (
 			<Switch>
-				<Route 
-					exact 
-					path="/" 	
-					render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps} />} />
+				<Route
+					exact
+					path="/"
+					render={(routeProps) => (
+						<PaletteList palettes={seedColors} {...routeProps} />
+					)}
+				/>
 				<Route
 					exact
 					path="/palette/:id"
@@ -28,6 +31,11 @@ class App extends Component {
 							)}
 						/>
 					)}
+				/>
+				<Route
+					exact
+					path='/palette/:paletteId/:colorId'
+					render={() => <h1>HI</h1>}
 				/>
 			</Switch>
 
