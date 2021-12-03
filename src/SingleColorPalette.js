882b6da63d6b2805b8ddc2fbd7086ff7fc4eeb8e
Russ Safin <russ.safin@gmail.com>
2021-12-03 03:48:30 -0800

    add GoBack button

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 import Navbar from './Navbar';
 import ColorBox from './ColorBox';
 import PaletteFooter from './PaletteFooter';
@@ -8,8 +9,8 @@ export default class SingleColorPalette extends Component {
 	constructor(props) {
 		super(props);
 		this._shades = this.gatherShades(this.props.palette, this.props.colorId);
-        this.state = {format: 'hex'};
-        this.changeFormat = this.changeFormat.bind(this);
+		this.state = { format: 'hex' };
+		this.changeFormat = this.changeFormat.bind(this);
 	}
 	gatherShades(palette, colorToFilterBy) {
 		let shades = [];
@@ -22,25 +23,30 @@ export default class SingleColorPalette extends Component {
 
 		return shades.slice(1);
 	}
-    changeFormat(val) {
+	changeFormat(val) {
 		this.setState({ format: val });
 	}
 	render() {
-        const {paletteName, emoji} = this.props.palette;
-        const {format} = this.state;
+		const { paletteName, emoji, id } = this.props.palette;
+		const { format } = this.state;
 		const colorBoxes = this._shades.map((color) => (
 			<ColorBox
-				key={color.id}
+				key={color.name}
 				name={color.name}
 				background={color[format]}
 				showLink={false}
 			/>
 		));
 		return (
-			<div className='Palette'>
-                <Navbar handleChange={this.changeFormat} isAllColors={false}/>
-				<div className='Palette-colors'>{colorBoxes}</div>
-                <PaletteFooter paletteName={paletteName} emoji={emoji} />
+			<div className="SingleColorPalette Palette">
+				<Navbar handleChange={this.changeFormat} isAllColors={false} />
+				<div className="Palette-colors">
+					{colorBoxes}
+					<div className="go-back ColorBox">
+						<Link to={`/palette/${id}`} className='back-button'>GO BACK</Link>
+					</div>
+				</div>
+				<PaletteFooter paletteName={paletteName} emoji={emoji} />
 			</div>
 		);
 	}
