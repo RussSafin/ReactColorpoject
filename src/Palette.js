ba8bcc66b7b1bc3d37e475262916930da3d7efd7
Russ Safin <russ.safin@gmail.com>
2021-12-04 03:45:35 -0800

    Move all styles in separate folder

--- src/Palette.js
+++ src/Palette.js
@@ -2,28 +2,12 @@ import React, { Component } from 'react';
 import ColorBox from './ColorBox';
 import Navbar from './Navbar';
 import PaletteFooter from './PaletteFooter';
+import styles from './styles/PaletteStyles';
 import { withStyles } from '@mui/styles';
-import './Palette.css';
 
 
-const styles = {
-	Palette: {
-		height: '100vh',
-		display: 'flex',
-		flexDirection: 'column',
-	},
-	Colors: {
-		height: '90%'
-	},
-	PaletteFootter: {
-		backgroundColor: 'white',
-		height: '5vh',
-		display: 'flex',
-		justifyContent: 'flex-end',
-		alignItems: 'center',
-		fontWeight: 'bold',
-	}
-};
+
+
 
 class Palette extends Component {
 	constructor(props) {
