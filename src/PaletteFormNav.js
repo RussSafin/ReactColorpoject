c16945f7a39ef7e98709690cf6d5aef2d09c2dcd
Russ Safin <russ.safin@gmail.com>
2021-12-15 04:25:14 -0800

    Make Palette List and all the components responsive

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import styles from './styles/PaletteFormNavStyles';
 import { withStyles } from '@material-ui/core/styles';
 import PaletteMetaForm from './PaletteMetaForm';
 import { Link } from 'react-router-dom';
@@ -10,44 +11,9 @@ import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Button from '@material-ui/core/Button';
-import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
-const drawerWidth = 400;
-const styles = (theme) => ({
-	root: {
-		display: 'flex',
-	},
-	appBar: {
-		transition: theme.transitions.create(['margin', 'width'], {
-			easing: theme.transitions.easing.sharp,
-			duration: theme.transitions.duration.leavingScreen,
-		}),
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		alignItems: 'center',
-		height: '64px',
-	},
-	appBarShift: {
-		width: `calc(100% - ${drawerWidth}px)`,
-		marginLeft: drawerWidth,
-		transition: theme.transitions.create(['margin', 'width'], {
-			easing: theme.transitions.easing.easeOut,
-			duration: theme.transitions.duration.enteringScreen,
-		}),
-	},
-	menuButton: {
-		marginLeft: 12,
-		marginRight: 20,
-	},
-	navBtns: {
-		marginRight: '1rem',
-		'& a': {
-			textDecoration: 'none',
-		},
-	},
-	button: {
-		margin: '0 0.5rem',
-	},
-});
+// import { ValidatorForm, TextValidator } from 'react-material-ui-form-validator';
+
+
 class PaletteFormNav extends Component {
 	constructor(props) {
 		super(props);
@@ -70,7 +36,7 @@ class PaletteFormNav extends Component {
 	}
 	render() {
 		const { classes, open, palettes, handleSubmit } = this.props;
-		const { newPaletteName } = this.state;
+		// const { newPaletteName } = this.state;
 		return (
 			<div className={classes.root}>
 				<CssBaseline />
